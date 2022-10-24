c9224380db4977e8d3e0b0a36634408d6db4862b
andreagiodi <andrea.giodice5@gmail.com>
2022-10-24 07:14:34 +0000

    commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('search.html')
+
+@app.route('/result', methods=['GET'])
+def result():
+    # Collegamento al database
+    import pandas as pd
+    import pymssql
+    import matplotlib.pyplot as plt
+    conn = pymssql.connect(server='213.140.22.237\SQLEXPRESS', user='giodice.andrea',                                        password='CarloCracco1962', database='giodice.andrea')
+    # Invio query al database e ricezioni informazioni
+    nomeProdotto = request.args['NomeProdotto']
+    query = f"SELECT * FROM production.products WHERE product_name LIKE '{nomeProdotto}%' "  
+    # Visualizzare informazioni
+    
+    return render_template('search.html')
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
