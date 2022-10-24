87fbc28cc45ea8650e76ff13793d6ef36b0314d6
andreagiodi <andrea.giodice5@gmail.com>
2022-10-24 07:37:06 +0000

    commit

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ def result():
     conn = pymssql.connect(server='213.140.22.237\SQLEXPRESS', user='giodice.andrea',                                        password='CarloCracco1962', database='giodice.andrea')
     # Invio query al database e ricezioni informazioni
     nomeProdotto = request.args['NomeProdotto']
-    query = f"SELECT * FROM production.products WHERE product_name LIKE '{nomeProdotto}%' "  
+    query = f"SELECT * FROM production.products WHERE product_name LIKE '{nomeProdotto}%' "
+    dfProdotti = pd.read_sql(query,conn)
     # Visualizzare informazioni
-    
-    return render_template('search.html')
+    return render_template('result.html',table = dfProdotti.to_html())
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
