1fd2ffb3d0298af925c6e99501b5043c5c314a67
andreagiodi <andrea.giodice5@gmail.com>
2022-10-28 07:48:03 +0000

    commit

---
+++ ProgettoInformatica/Documentazione.md
@@ -0,0 +1,2 @@
+Si intende realizzare un sito web che tratti i dati degli stadi di calcio in tutto il mondo.
+Questi dati saranno caricati tramite un database
--- ProgettoInformatica/app.py
+++ ProgettoInformatica/app.py
@@ -8,21 +8,25 @@ from flask import (
     current_app
 )
 app = Flask(__name__)
-import urllib.request 
-from urllib.parse import urlparse,urljoin
-from bs4 import BeautifulSoup
-import requests,validators,json ,uuid,pathlib,os
+from urllib.request import urlopen
 import pandas as pd
 import pymssql
 import matplotlib.pyplot as plt
 conn = pymssql.connect(server='213.140.22.237\SQLEXPRESS', user='giodice.andrea', password='CarloCracco1962', database='giodice.andrea')
 
+url = "http://stadiumdb.com"
 
 
 @app.route('/', methods=['GET'])
 def home():
-        
-    return render_template('home.html')
+    page = urlopen(url)
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+    title_index = html.find("<main>")
+    start_index = title_index + len("<main>")
+    end_index = html.find("</main>")
+    title = html[start_index:end_index]
+    return render_template('home.html', title=title)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
