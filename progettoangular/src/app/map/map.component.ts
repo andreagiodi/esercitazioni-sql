c216f8942d08d8ae5048f3a2b6f0fcad1bb8d1d6
andreagiodi <andrea.giodice5@gmail.com>
2022-11-10 08:32:41 +0000

    commit

--- progettoangular/src/app/map/map.component.ts
+++ progettoangular/src/app/map/map.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component } from '@angular/core';
-import * as L from 'leaflet'
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -9,12 +9,22 @@ import * as L from 'leaflet'
 export class MapComponent implements AfterViewInit {
   private map;
 
-  constructor() { }
+  constructor() { 
+  }
 
   ngAfterViewInit(): void {
     this.initMap();
   }
 
-  private initMap(): void {}
+  private initMap(): void {
+
+    this.map = L.map('map').setView([32.339444, -6.360833], 15);
+
+    const tiles = L.tileLayer('https://tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png', {
+      maxZoom: 20
+    });
+
+    tiles.addTo(this.map);
+  }
 
 }
